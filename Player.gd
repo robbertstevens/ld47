f820846bae2ab6807fb8d15ea1645a68b2f7ed1a
Robbert Stevens <robbertstevens@gmail.com>
2020-10-03 13:00:26 +0200

    Remove old player

--- Player.gd
+++ Player.gd
@@ -1,19 +1,36 @@
-extends PathFollow2D
+extends Sprite
 
-var current_speed = 0;
-var speed_increment = 5
-var max_speed = 100;
+onready var Track = get_parent()
 
+var last_move = 0;
+var movespeed = .5;
+var track = []
+
+func _ready():
+	track = [
+		Vector2(0,0),
+		Vector2(1,0),
+		Vector2(2,0),
+		Vector2(3,0),
+		Vector2(4,0),
+		Vector2(4,1),
+		Vector2(4,2),
+		Vector2(4,3),
+		Vector2(3,3),
+		Vector2(2,3),
+		Vector2(1,3),
+		Vector2(0,3),
+		Vector2(0,2),
+		Vector2(0,1),
+	]
 
 func _process(delta):
-	offset += delta * current_speed
-
-func _input(event):
-	if event.is_action("ui_up"):
-		current_speed += speed_increment
-		current_speed = min(max_speed, current_speed);
+	last_move -= delta;
 	
-	if event.is_action("ui_down"):
-		current_speed -= speed_increment
-		current_speed = max(0, current_speed);
-	
+	if last_move <= 0: 
+		var next_move = track.pop_front()
+		track.push_back(next_move)
+		
+		position = next_move*16
+		
+		last_move = movespeed
