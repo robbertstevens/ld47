d80382275ddc13af137eb5ef597c69c69ae7fb1b
Robbert Stevens <robbertstevens@gmail.com>
2020-10-03 17:11:29 +0200

    Add trainstation + bullets

--- Bullet.gd
+++ Bullet.gd
@@ -1,4 +1,21 @@
 extends Area2D
 
-func _ready():
-	pass 
+var SPEED = 2
+var BULLET_LIFE_TIME = 10
+
+var alive_for_seconds = 0
+
+var direction = Vector2.ZERO
+
+func set_dir(dir):
+	direction = dir
+
+func _process(delta):
+	alive_for_seconds += delta
+	
+	var motion = direction * SPEED
+	
+	position += motion * delta
+
+	if alive_for_seconds >= BULLET_LIFE_TIME:
+		queue_free()
--- Gun.gd
+++ Gun.gd
@@ -1,4 +1,4 @@
-extends Position2D
+extends Node2D
 
 const BULLET_SCENE = preload("res://Bullet.tscn")
 const SPEED = 100
@@ -8,7 +8,8 @@ func shoot():
 	var bullet = BULLET_SCENE.instance()
 	find_parent("World").add_child(bullet)
 	bullet.position = get_global_transform().get_origin()
+	bullet.set_dir((get_global_mouse_position() - get_global_transform().get_origin()))
 
 func _input(event):
-	if event.is_action_pressed("ui_accept"):
+	if event.is_action("ui_accept"):
 		shoot()
