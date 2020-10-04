407acd06405cecf3ccf7d5ceb18b986207735c10
Robbert Stevens <robbertstevens@gmail.com>
2020-10-04 20:22:18 +0200

    Progress

--- player/MoveTrain.gd
+++ player/MoveTrain.gd
@@ -3,6 +3,14 @@ extends Path2D
 var GUN_SCENE = preload("res://player/gun/Gun.tscn")
 var current_speed = 0;
 
+func _process(delta):
+	var children = get_children()
+	
+	for child in children:
+		child.last = false
+		
+	children.back().last = true
+
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_up"): 
 		current_speed += delta
@@ -12,7 +20,7 @@ func _physics_process(delta):
 		current_speed = max(current_speed, -0.55)
 	
 	if Input.is_action_just_pressed("ui_load"):
-		var colliders = $Player.get_node("Area2D").get_overlapping_areas()
+		var colliders = $Player.get_node("PickupLayer").get_overlapping_areas()
 		
 		for collider in colliders:
 			if not collider.get_collision_layer() == 16:
@@ -21,10 +29,15 @@ func _physics_process(delta):
 			collider.get_parent().queue_free();
 			
 			var all_trains = get_children()
+			
+			for t in all_trains:
+				t.last = false
+				
 			var last_train = all_trains.back()
 			
 			var train = GUN_SCENE.instance()
 			train.offset = last_train.offset - 16
+			train.last = true
 			add_child(train)
 	
 	var children = get_children()
--- player/Player.gd
+++ player/Player.gd
@@ -1,11 +1,23 @@
 extends PathFollow2D
 
 var PASSENGER_SCENE = preload("res://passenger/Passenger.tscn")
+export var last:bool = false
+
+const MAX_HEALTH = 100
+
+var current_health = MAX_HEALTH
 
 var current_passenger = 0
 
-func	 _process(delta):
-	pass
+func _ready():
+	$Control/HealthBar.value = MAX_HEALTH
+	$Control/HealthBar.max_value = MAX_HEALTH
 
-func take_damage():
-	pass
+func	 _process(delta):
+	if current_health <= 0:
+		get_tree().paused = true 
+
+func take_damage(damage: int = 10):
+	if last: 
+		current_health -= damage
+		$Control/HealthBar.value = current_health
