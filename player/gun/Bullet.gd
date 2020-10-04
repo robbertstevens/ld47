b6a048ad29024ae809f7be6f467efbcbfe3c0bc0
Robbert Stevens <robbertstevens@gmail.com>
2020-10-04 11:32:43 +0200

    Add bullet explosion effect

--- player/gun/Bullet.gd
+++ player/gun/Bullet.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+onready var EXPLOSION_SCENE = preload("res://player/gun/Explosion.tscn")
+
 var BULLET_SPEED = 50
 var BULLET_LIFE_TIME = 10
 
@@ -26,5 +28,10 @@ func collision(collider: KinematicCollision2D):
 		return
 	
 	collider.collider.take_damage()
-	
+	spawn_explosion()
 	queue_free()
+
+func spawn_explosion():
+	var explosion = EXPLOSION_SCENE.instance()
+	find_parent("World").add_child(explosion)
+	explosion.position = get_global_transform().get_origin()
