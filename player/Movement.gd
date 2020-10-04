33d67f308f70a9acfedcec31da0a55d4d8e1cc0c
Robbert Stevens <robbertstevens@gmail.com>
2020-10-04 13:47:48 +0200

    THings

--- player/Movement.gd
+++ player/Movement.gd
@@ -1,8 +1,8 @@
-extends Path2D
+extends PathFollow2D
 
 var current_speed = 0;
 
-func _physics_process(delta):
+func _process(delta):
 	if Input.is_action_pressed("ui_up"): 
 		current_speed += delta
 		current_speed = min(current_speed, 0.55)
@@ -10,8 +10,5 @@ func _physics_process(delta):
 		current_speed -= delta
 		current_speed = max(current_speed, -0.55)
 	
-	var children = get_children()
-	
-	for child in children: 
-		child.offset += current_speed
+	offset += current_speed
 
--- player/Player.gd
+++ player/Player.gd
@@ -1,14 +1,3 @@
 extends PathFollow2D
 
-var current_speed = 0;
-
-func _process(delta):
-	if Input.is_action_pressed("ui_up"): 
-		current_speed += delta
-		current_speed = min(current_speed, 0.55)
-	elif	 Input.is_action_pressed("ui_down"):
-		current_speed -= delta
-		current_speed = max(current_speed, -0.55)
-	
-	offset += current_speed
 
